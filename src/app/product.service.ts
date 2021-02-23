c2979e7c6f45daa016b6473b0bbd558901ee3c29
Dominik Sasko <dominik.sasko@siemens-healthineers.com>
2021-02-23 15:00:31 +0100

    Third update of part 3

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -9,11 +9,8 @@ export class ProductService {
 
   private _albumUrl = "../assets/album.json";
 
-  getAlbum(id: number){
-    return this._http.get(this._albumUrl)
-    .map(response => {
-      return response.json();
-    });
+  getAlbum(id: number) {
+    return this._http.get(this._albumUrl).map((response) => response.json());
   }
 
 }
